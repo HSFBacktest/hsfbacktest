270bdd061c708b38dcbdc41d729286dc75bfcf70
HSFBacktest <hsfdirectors@gmail.com>
2016-04-25 12:31:00 -0400

    TimeSeriesData

--- timeSeriesData.py
+++ timeSeriesData.py
@@ -4,10 +4,20 @@ import datetime
 import pandas.io.data as web
 import matplotlib.pyplot as plt
 import datetime
-from matplotlib import style
 from itertools import product
+#from openpyxl import load_workbook
 
+df = pd.read_excel('DailyData.xlsx',sheetname='Consumer Discretionary',index_col='Date', 
+        parse_dates=True)
+df.index
+start = datetime.datetime(2014,1,2)
+end = datetime.datetime(2014,12,31)
 
-df = pd.read_csv('dailyReturns.csv',index_col=0)
+df2 = df[start:end]
 
-print(df)
+print(df2.head())
+
+'''
+dfs = {sheet_name: xl_file.parse(sheet_name)
+        for sheet_name in xl_file.sheet_names}
+'''
