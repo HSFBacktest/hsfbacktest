1c763e8dcef0e6f03d7f19ff8604dc47fa446d18
HSFBacktest <hsfdirectors@gmail.com>
2016-06-15 18:50:24 -0400

    Test Commit

--- main V2.py
+++ main V2.py
@@ -8,4 +8,17 @@ from itertools import product
 df = pd.read_csv('ATTMonthlyDataVALUES.csv',
     index_col=0)
 
-print(df.head())
+price2 = df['PRICE']
+df.insert(0,'PRICE2',price2)
+
+df.PRICE2 = df.PRICE2.shift(-1)
+
+priceReturn = df['PRICE2'] - df['PRICE']
+df.insert(0,'RETURN',priceReturn)
+
+returnPercent = df['RETURN']/df['PRICE']*100
+df.insert(0,'PERCENTRETURN',returnPercent)
+
+#print(df)
+print(df.index)
+
